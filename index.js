e23d335364947f3417542b85e95964d562ef04ec
AJ Javadi <amirjavadi25@gmail.com>
2023-06-14 21:27:36 -0500

    made updates to index.js, svg.js, shapes.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// import our required moduels
+const inquirer = require("inquirer") 
+const fs = require("fs")
+const svg = require("./lib/svg") //imports our svg.js file
+const {Circle, Square, Triange }  = require("./lib/shapes")// import from shapes.js 
+
+
+//pseudocode
+
+/****************
+ * 
+ * 
+ * const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+ * 
+ * 
+ */
