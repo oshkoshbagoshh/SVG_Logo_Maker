bc98be03c8ffc3d6545a7f50431964e69e24cc14
AJ <amirjavadi25@gmail.com>
2024-01-11 16:54:50 -0600

    updated svg and shapes files

--- index.js
+++ index.js
@@ -1,59 +1,18 @@
-// Import required modules
-const inquirer = require("inquirer");
-const fs = require("fs");
-const {SVG} = require("./lib/svg");
-const { Circle, Square, Triangle } = require("./lib/shapes");
+/*
+ * @Author: AJ Javadi 
+ * @Email: amirjavadi25@gmail.com
+ * @Date: 2024-01-11 16:43:51 
+ * @Last Modified by: Someone
+ * @Last Modified time: 2024-01-11 16:44:33
+ * @Description: file:///Users/aj/Desktop/bootcamp/Homework/8.%20Logo%20Maker/SVG_Logo_Maker/index.js
+ * run the CLI module
+ */
 
-// Define the array of questions using inquirer
-const questions = [
-  {
-    type: 'input',
-    message: "Please enter the logo text...",
-    name: 'text',
-  },
-  {
-    type: 'input',
-    message: 'Please enter the text color...',
-    name: 'textcolor',
-  },
-  {
-    type: 'list',
-    message: 'Please select a logo shape...',
-    name: 'shape',
-    choices: ['circle', 'square', 'triangle'],
-  },
-  {
-    type: 'input',
-    message: 'Please enter a logo color...',
-    name: 'shapecolor',
-  }
-];
+const CLI = require("./lib/cli");
 
-// Prompt the questions using inquirer
-inquirer.prompt(questions).then(function({ text, textcolor, shape, shapecolor }) {
-  let shapetype = null;
+// run the CLI module
+new CLI().run();
 
-  switch (shape) {
-    case 'circle':
-      shapetype = new Circle(shapecolor);
-      break;
-    case 'square':
-      shapetype = new Square(shapecolor);
-      break;
-    case 'triangle':
-      shapetype = new Triangle(shapecolor);
-      break;
-  }
+// Path: lib/cli.js
 
-  const svg = new SVG();
-  svg.setText(text, textcolor);
-  svg.setShape(shapetype);
 
-  fs.writeFile('logo.svg', svg.render(), function(err) {
-    if (err) {
-      console.log('Error writing file!');
-    } else {
-      console.log('File saved successfully as logo.svg!');
-    }
-  });
-});
